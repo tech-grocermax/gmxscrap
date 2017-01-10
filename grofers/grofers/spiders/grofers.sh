4f5780a25012982c97f8ef1acaa2b2834a663cff
Pradeep Gupta <pradeep.gupta@grocermax.com>
2017-01-10 18:11:00 +0530

    - added shell script to crawl price.

---
+++ grofers/grofers/spiders/grofers.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#cd /home/pradeep/code/applications/gmxscrap/bigbasket/bigbasket/spiders
+#echo "pwd: `pwd`"
+#echo "\$0: $0"
+#echo "basename: `basename $0`"
+#echo "dirname: `dirname $0`"
+scriptPath="$(dirname $(readlink -f $0))"
+#echo $scriptPath
+cd $scriptPath
+PATH=$PATH:/usr/local/bin
+export PATH
+scrapy crawl grofers
